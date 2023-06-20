2914b5dd6cf5f6fb898e75d1942714bfdaf0f9db
Stray2222 <nnu2150@gmail.com>
2023-06-20 14:54:18 +0500

    WAit

--- src/main/java/ru/kata/spring/boot_security/demo/models/User.java
+++ src/main/java/ru/kata/spring/boot_security/demo/models/User.java
@@ -3,6 +3,8 @@ package ru.kata.spring.boot_security.demo.models;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
@@ -48,7 +50,8 @@ public class User implements UserDetails {
     @Transient
     private String passwordConfirm;
 
-    @ManyToMany(fetch = FetchType.EAGER)
+    @ManyToMany(fetch=FetchType.LAZY)
+    @Fetch(FetchMode.JOIN)
     private Set<Role> roles;
 
     @Override
